bdd4bb7678d2d68cff5d24b1dc90226542f5b70f
ReginaS1987 <reginasirazieva1987@gmail.com>
2024-02-14 12:30:05 +0300

    add calc, add aditional data to the form

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,6 +1,7 @@
 const modals = ()=> {
-function bindModal(triggerSeletor, modalSelector, closeSelector) {
+function bindModal(triggerSeletor, modalSelector, closeSelector, closeClickOverlay= true) {
     const trigger = document.querySelectorAll(triggerSeletor),
+          window=document.querySelectorAll('[data-modal]'),
           modal=document.querySelector(modalSelector),
           close = document.querySelector(closeSelector);  
 
@@ -9,6 +10,10 @@ trigger.forEach(item=> {
         if(e.target) {
             e.preventDefault();
         }
+        window.forEach(item=> {
+            item.style.display='none';
+
+        });
         modal.style.display='block';
         document.body.style.overflow='hidden';
         // document.body.classList.add('modal-open');
@@ -20,12 +25,20 @@ close.addEventListener('click',() => {
     modal.style.display='none';
 
     document.body.style.overflow='';
+    window.forEach(item=> {
+        item.style.display='none';
+
+    });
 });
 
 modal.addEventListener('click', (e)=> {
-    if(e.target===modal) {
+    if(e.target===modal && closeClickOverlay) {
         document.body.style.overflow='';
         modal.style.display='none';
+        window.forEach(item=> {
+            item.style.display='none';
+
+        });
 
         // document.body.classList.remove('modal-open');
     }
@@ -44,6 +57,9 @@ function showModalByTime(selector,time) {
 }
 bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 bindModal('.phone_link', '.popup', '.popup .popup_close');
+bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 // showModalByTime('.popup', 3000);
 };
 export default modals;
